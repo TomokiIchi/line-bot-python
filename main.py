052055b59a159b405f7235677ba36ac96f820ba4
tomoki.ichikawa <tomoki.ichikawa@Ichikawa.local>
2020-11-08 21:52:13 +0900

    fist commit

--- main.py
+++ main.py
@@ -8,8 +8,23 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessalsge,
 )
 
+app = Flask(__name__)
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
+YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
+
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+
+@app.route("/")
+def hello_world():
+    return "Hello World!"
+
+@app.route("callback", methods=['POST'])
+def callback():
+    sinature = request.headers['X-Line-Signature']
+
 if __name__ == "__main__":
-    port
+    app.run()
