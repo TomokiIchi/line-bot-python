aaff806860c542656c35c4fd395cb4e605def80e
tomoki.ichikawa <tomoki.ichikawa@Ichikawa.local>
2020-11-08 20:01:50 +0900

    import completed

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 from flask import Flask, request, abort
+import os
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+
+if __name__ == "__main__":
+    port
